d34a63fe4b46dd92e8219b1e8e525dc97a67aeac
Alex Conway <abconway2@gmail.com>
2024-02-26 15:38:42 -0500

    add a dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,16 +5,20 @@ class_name Player
 @onready var animation_tree := $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 @onready var sword_collision_shape_2d = $Marker2D/Sword/CollisionShape2D
+@onready var dash_timer = $DashTimer
+@onready var dash_particles = $DashParticles
 
-const SPEED := 100
+var speed := 100
 
 var previous_direction: Vector2
 var is_attacking = false
+var is_dashing = false
 var health = 3
 
 
 func _ready() -> void:
 	sword_collision_shape_2d.disabled = true
+	dash_timer.timeout.connect(dash_complete)
 
 
 func _physics_process(delta: float) -> void:
@@ -23,6 +27,12 @@ func _physics_process(delta: float) -> void:
 	if not is_attacking and Input.is_action_just_pressed("player_attack"):
 		is_attacking = true
 		playback.travel("attack")
+	elif not is_attacking and Input.is_action_just_pressed("player_dash"):
+		if not is_dashing:
+			speed *= 2
+			is_dashing = true
+			dash_timer.start()
+			dash_particles.emitting = true
 	elif not is_attacking:
 		var direction := Vector2(
 			Input.get_axis("player_move_left", "player_move_right"),
@@ -35,7 +45,7 @@ func _physics_process(delta: float) -> void:
 			playback.travel("walk")
 			previous_direction = direction
 			set_blend_pos(direction)
-		velocity = direction * SPEED
+		velocity = direction * speed
 		move_and_slide()
 
 
@@ -48,6 +58,12 @@ func set_blend_pos(p: Vector2) -> void:
 func attack_complete() -> void:
 	is_attacking = false
 	playback.travel("idle")
+
+
+func dash_complete() -> void:
+	dash_particles.emitting = false
+	is_dashing = false
+	speed /= 2
 
 
 func _on_sword_body_entered(body):
